46551bb1b61761fe39db105a79781b68ce8d6b2f
John Dieter <johndieter123@gmail.com>
2019-06-18 17:03:53 -0500

    Created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+console.log("Up and running!");
+var card1 = "Queen";
+var card2 = "King";
+var card3 = "King";
+var card4 = "Queen";
+console.log("User flipped " + card1);
+console.log("User flipped " + card2);
